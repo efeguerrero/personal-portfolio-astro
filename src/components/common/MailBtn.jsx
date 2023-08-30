766c5bb7fd1e76ac70f202b48e052ffd8bac1430
efeguerrero <juanfg.8640@gmail.com>
2023-08-30 11:12:11 -0300

    style[mailBtn]: converted to tailwind

--- src/components/common/MailBtn.jsx
+++ src/components/common/MailBtn.jsx
@@ -7,7 +7,7 @@ const MailBtn = () => {
   return (
     <a
       href="mailto:fr?ncisco.guerrero.d??vATgmailDOTcom"
-      className={styles.mailBtn}
+      className="bg-transparent text-alpha border border-alpha w-max px-14 py-3 text-lg font-bold tracking-widest relative transition-all duration-300 ease-in-out capitalize md:text-xl after:origin-left after:absolute after:inset-0 after:bg-alpha after:z-[-1] after:transition-all after:duration-300 after:ease-in-out after:scale-x-0 lg:hover:after:scale-x-100 lg:hover:text-white"
       onClick={(e) =>
         (e.target.href = e.target.href
           .replace('AT', '@')
