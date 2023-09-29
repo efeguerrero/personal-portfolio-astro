818dd2ca9c446a2e7876bd94b11d25545957fca9
efeguerrero <juanfg.8640@gmail.com>
2023-09-29 19:36:13 -0300

    checkpoint header new style

--- src/assets/icons/ReactIcon.jsx
+++ src/assets/icons/ReactIcon.jsx
@@ -2,6 +2,7 @@
 import { ReactComponent as ChevronDown } from './system/chevronDown.svg';
 import { ReactComponent as Github } from './social/github.svg';
 import { ReactComponent as Linkedin } from './social/linkedin.svg';
+import { ReactComponent as Twitter } from './social/twitter.svg';
 
 const ReactIcon = ({ className, icon }) => {
   return (
@@ -9,6 +10,7 @@ const ReactIcon = ({ className, icon }) => {
       {icon == 'chevronDown' && <ChevronDown />}
       {icon == 'github' && <Github />}
       {icon == 'linkedin' && <Linkedin />}
+      {icon == 'twitter' && <Twitter />}
     </div>
   );
 };
