800f8da3e648b6148576f299ede9e2ffb25b4ee1
efeguerrero <juanfg.8640@gmail.com>
2023-10-05 11:50:35 -0300

    pending add email hiding

--- src/assets/icons/ReactIcon.jsx
+++ src/assets/icons/ReactIcon.jsx
@@ -3,6 +3,9 @@
 import { ReactComponent as Pointer } from './system/pointer.svg';
 import { ReactComponent as Github } from './social/github.svg';
 import { ReactComponent as OpenInNew } from './system/openInNew.svg';
+import { ReactComponent as Twitter } from './social/twitter.svg';
+import { ReactComponent as Linkedin } from './social/linkedin.svg';
+import { ReactComponent as Email } from './social/email.svg';
 
 const ReactIcon = ({ className, icon }) => {
   return (
@@ -10,6 +13,9 @@ const ReactIcon = ({ className, icon }) => {
       {icon == 'pointer' && <Pointer />}
       {icon == 'github' && <Github />}
       {icon == 'openInNew' && <OpenInNew />}
+      {icon == 'twitter' && <Twitter />}
+      {icon == 'linkedin' && <Linkedin />}
+      {icon == 'email' && <Email />}
     </div>
   );
 };
