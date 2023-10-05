20b0461fb0cdcb9c954a85f31e2fb0e6ebce97aa
efeguerrero <juanfg.8640@gmail.com>
2023-10-04 21:33:56 -0300

    new Nav

--- src/components/common/Container.jsx
+++ src/components/common/Container.jsx
@@ -1,6 +1,6 @@
 const Container = ({ children }) => {
   return (
-    <div className="mx-auto max-w-7xl">
+    <div className="mx-auto max-w-2xl">
       <div className="container px-6 2xl:p-0">{children}</div>
     </div>
   );
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -9,12 +9,10 @@ import { motion, AnimatePresence } from 'framer-motion';
 
 //Component Imports
 import Container from '@/components/common/Container.jsx';
-import ReactIcon from '../../assets/icons/ReactIcon.jsx';
 
 const navigation = [
   { name: 'About', href: '#about' },
   { name: 'Projects', href: '#projects' },
-  { name: 'Contact', href: '#contact' },
 ];
 
 const Nav = () => {
@@ -83,150 +81,15 @@ const Nav = () => {
   };
 
   return (
-    <section className="absolute pb-8 pt-10 lg:pt-14 inset-x-0 top-0 lg:z-20">
+    <section className="absolute pb-8 pt-14 inset-x-0 top-0">
       <Container>
-        <nav className=" w-full flex items-center justify-between bg-transparent">
-          <a
-            href="/"
-            className="cursor-pointer z-20 mt-[2px] text-2xl hover:text-neutral-300 transition-all font-normal tracking-widest  text-neutral-400"
-          >
-            {/* <ReactIcon icon="logo" className="w-10 h-10 text-neutral-600" /> */}
-            {`{fg}`}
-          </a>
-
-          <Collapsible.Root open={isMenuOpen} onOpenChange={handleMenuClick}>
-            <Collapsible.Trigger asChild>
-              <motion.div
-                variants={trigger}
-                initial="initial"
-                animate={isMenuOpen ? 'menuIcon' : 'initial'}
-                id="menuIcon"
-                className="lg:hidden tapColor-none relative [--translateInitial:1px] z-20 flex flex-col gap-[6px] items-center group h-full w-[1.8rem] cursor-pointer "
-              >
-                <motion.div
-                  variants={trigger}
-                  initial="initial"
-                  animate={isMenuOpen ? 'activated' : 'initial'}
-                  className="h-[2px] w-[1.5rem] rounded-sm bg-neutral-400 [--rotate:-45deg] origin-right  [--translate:-50%] "
-                />
-                <motion.div
-                  variants={trigger}
-                  initial="initial"
-                  animate={isMenuOpen ? 'hidden' : 'initial'}
-                  className="h-[2px] w-[1.5rem] rounded-sm bg-neutral-400 "
-                />
-                <motion.div
-                  variants={trigger}
-                  initial="initial"
-                  animate={isMenuOpen ? 'activated' : 'initial'}
-                  className="h-[2px] w-[1.5rem] rounded-sm bg-neutral-400 [--rotate:45deg] origin-right  [--translate:50%] "
-                />
-              </motion.div>
-            </Collapsible.Trigger>
-            {/* Dialog */}
-            <AnimatePresence>
-              {isMenuOpen && (
-                <Portal.Root asChild>
-                  <Collapsible.Content forceMount asChild>
-                    <motion.div
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: 1 }}
-                      exit={{ opacity: 0 }}
-                      className="fixed bottom-0 z-10 w-full bg-neutral-900"
-                    >
-                      <Container>
-                        <section className="relative w-full grid grid-rows-[1fr_3fr_1fr] h-[100dvh] items-center justify-center ">
-                          <div className="row-start-2 w-full flex flex-col gap-6 items-center justify-start">
-                            {navigation.map((item, index) => {
-                              return (
-                                <motion.div
-                                  key={index}
-                                  whileHover="hover"
-                                  initial={{
-                                    y: 'var(--translate-wide,0)',
-                                    opacity: 'var(--opacity-small,1)',
-                                  }}
-                                  animate={{ y: 0, opacity: 1 }}
-                                  transition={{
-                                    opacity: {
-                                      type: 'tween',
-                                      duration: 1,
-                                      delay: 0.3 + index / 5,
-                                    },
-                                    y: {
-                                      type: 'tween',
-                                      duration: 0.8,
-                                      delay: 0.2 - index / 10,
-                                    },
-                                  }}
-                                  className="relative my-4 flex items-center gap-6 lg:[--translate-wide:-100vw] [--opacity-small:0] lg:[--opacity-small:1]"
-                                >
-                                  <motion.a
-                                    variants={{
-                                      hover: {
-                                        letterSpacing: '5px',
-                                      },
-                                    }}
-                                    href={item.href}
-                                    onClick={() => handleMenuClick()}
-                                    className=" text-neutral-300 font-extralight leading-none tracking-tighter z-10 text-4xl "
-                                  >
-                                    {item.name}
-                                  </motion.a>
-                                </motion.div>
-                              );
-                            })}
-                          </div>
-                          <div class="row-start-3 w-full flex lg:flex-row gap-10 justify-between items-start">
-                            <motion.a
-                              variants={socialLinks}
-                              initial="initial"
-                              animate="animate"
-                              custom={0.3}
-                              class="text-neutral-400 uppercase font-light text-sm tracking-tight underline underline-offset-4"
-                              target="_blank"
-                              href="https://twitter.com/frnguerrero"
-                            >
-                              Twitter
-                            </motion.a>
-                            <motion.a
-                              variants={socialLinks}
-                              initial="initial"
-                              animate="animate"
-                              custom={0.5}
-                              class="text-neutral-400 uppercase font-light text-sm tracking-tight underline underline-offset-4"
-                              target="_blank"
-                              href="https://www.linkedin.com/in/juan-francisco-guerrero-16143849/?locale=en_US"
-                            >
-                              Linkedin
-                            </motion.a>
-                            <motion.a
-                              variants={socialLinks}
-                              initial="initial"
-                              animate="animate"
-                              custom={0.7}
-                              class="text-neutral-400 uppercase font-light text-sm tracking-tight underline underline-offset-4"
-                              target="_blank"
-                              href="https://github.com/efeguerrero"
-                            >
-                              Github
-                            </motion.a>
-                          </div>
-                        </section>
-                      </Container>
-                    </motion.div>
-                  </Collapsible.Content>
-                </Portal.Root>
-              )}
-            </AnimatePresence>
-          </Collapsible.Root>
-          {/* lg screen nav */}
-          <div className="hidden lg:flex gap-10 justify-center items-center">
+        <nav className=" w-full  bg-transparent">
+          <div className="flex gap-8 justify-start lg:justify-center items-center">
             {navigation.map((item, index) => {
               return (
                 <a
                   key={index}
-                  className="text-neutral-300 uppercase font-light text-sm tracking-[4px] cursor-pointer hover:text-neutral-50 transition-colors duration-200 ease-in-out"
+                  className="text-neutral-300 lowercase  text-lg  cursor-pointer hover:text-neutral-50 transition-colors duration-200 ease-in-out"
                   href={item.href}
                 >
                   {item.name}
