548ed1fd2294ea260a38b2dab1c90ee0d25d35e1
efeguerrero <juanfg.8640@gmail.com>
2023-10-04 22:24:56 -0300

    new rad lines

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,12 +1,3 @@
-import { useState } from 'react';
-
-//Radix Import
-import * as Collapsible from '@radix-ui/react-collapsible';
-import * as Portal from '@radix-ui/react-portal';
-
-//Framer Motion Imports
-import { motion, AnimatePresence } from 'framer-motion';
-
 //Component Imports
 import Container from '@/components/common/Container.jsx';
 
@@ -16,75 +7,11 @@ const navigation = [
 ];
 
 const Nav = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const handleMenuClick = () => {
-    setIsMenuOpen(!isMenuOpen);
-    document.body.classList.toggle('overflow-hidden');
-    document.body.classList.toggle('overscroll-behavior-contain');
-  };
-
-  //Framer Motion Variants
-  const trigger = {
-    initial: {
-      rotate: 0,
-      translateY: 0,
-
-      transition: {
-        type: 'tween',
-        ease: 'easeInOut',
-        duration: 0.3,
-      },
-    },
-
-    menuIcon: {
-      rotate: 90,
-      transition: {
-        type: 'tween',
-        ease: 'easeInOut',
-        duration: 0.3,
-      },
-    },
-
-    hidden: {
-      scale: 0,
-      transition: {
-        type: 'tween',
-        ease: 'easeInOut',
-        duration: 0.3,
-      },
-    },
-    activated: {
-      rotate: 'var(--rotate)',
-      translateY: 'var(--translate)',
-      transition: {
-        type: 'tween',
-        ease: 'easeInOut',
-        duration: 0.3,
-      },
-    },
-  };
-
-  const socialLinks = {
-    initial: {
-      opacity: 0,
-      y: 100,
-    },
-    animate: (customDelay) => ({
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.5,
-        delay: customDelay,
-      },
-    }),
-  };
-
   return (
     <section className="absolute pb-8 pt-14 inset-x-0 top-0">
       <Container>
         <nav className=" w-full  bg-transparent">
-          <div className="flex gap-8 justify-start lg:justify-center items-center">
+          <div className="flex gap-8 justify-start md:justify-center items-center">
             {navigation.map((item, index) => {
               return (
                 <a
