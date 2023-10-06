7f7f574a40ff3d285db32ce4691013245732e296
efeguerrero <juanfg.8640@gmail.com>
2023-10-06 15:47:14 -0300

    new project data

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -23,29 +23,28 @@ const projects = [
       'Product catalogue website built for Stefanos Leather. Built with NextJS as a static site for optimized performance and Contentful CMS for catalogue management. ',
   },
   {
-    name: 'tints & shades',
-    demoURL: 'https://shade-your-color.netlify.app',
-    githubURL: 'https://github.com/efeguerrero/shade-your-color.git',
-    technologies: ['react', 'javascript'],
-    description:
-      'Generate tints and shades for any colour to upgrade your palette. Built using Values.js from Noel Delgado.',
-  },
-  {
-    name: 'do it',
-    demoURL: 'https://reactjs-project-to-do-list.netlify.app',
-    githubURL: 'none',
-    technologies: ['react', 'javascript', 'PWA'],
-    description:
-      'Simple to-do app built with react. All data is stored locally on your device and no registration is needed. Developed as a Progressive Web App to give mobile users a more native experience.  ',
-  },
-  {
     name: 'crece mas',
     demoURL: 'https://www.crecemas.net/en/',
     githubURL: 'https://github.com/efeguerrero/crece-web',
     technologies: ['astro', 'javascript', 'tailwind', 'framer motion', 'radix'],
     description: 'Website for argentinean digital marketing agency.  ',
   },
-
+  {
+    name: 'tints & shades',
+    demoURL: 'https://shade-your-color.netlify.app',
+    githubURL: 'https://github.com/efeguerrero/shade-your-color.git',
+    technologies: ['react', 'javascript'],
+    description:
+      'Generate tints and shades for any colour to upgrade your palette. Built as part of a React Course using Values.js from Noel Delgado.',
+  },
+  {
+    name: 'do it',
+    demoURL: 'https://reactjs-project-to-do-list.netlify.app',
+    githubURL: 'https://github.com/efeguerrero/to-do-app',
+    technologies: ['react', 'javascript', 'PWA'],
+    description:
+      'Simple to-do app built as part of a React Course. All data is stored locally on your device and no registration is needed. Developed as a Progressive Web App to give mobile users a more native experience.  ',
+  },
   {
     name: 'Brasa Agency',
     demoURL: 'https://brasa.agency/',
