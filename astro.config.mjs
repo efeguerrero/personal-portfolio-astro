330a1a2cff9176524688fd2ce99008a19616d9a5
efeguerrero <juanfg.8640@gmail.com>
2023-10-06 16:51:48 -0300

    vercel analytics added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import svgr from 'vite-plugin-svgr';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 import react from '@astrojs/react';
@@ -9,6 +10,12 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [react(), tailwind()],
   vite: {
     plugins: [svgr()],
